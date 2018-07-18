2cbc15d37e49cada03ac572d3d5c88566a2c8e6a
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 14:45:50 +0100

    addition of sum array

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array_1, array_2)
   return array_1.length() + array_2.length()
 end
+
+def sum_array(numbers_array)
+  total = 0
+  for numbers in numbers_array
+    total += numbers
+  end
+  return total
+end
